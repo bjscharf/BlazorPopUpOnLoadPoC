8e4e15c4d79a1e7f220823c4633ce9dd297bbcf5
Bill Scharf <sto2868@wvsto.com>
2025-08-15 16:10:58 -0400

    working spinner with WVSTO logo

--- Misc/SpinnerService.cs
+++ Misc/SpinnerService.cs
@@ -10,8 +10,8 @@
 
     public class SpinnerService
     {
-        public event Action OnShow;
-        public event Action OnHide;
+        public event Action? OnShow;
+        public event Action? OnHide;
 
         public void Show()
         {
